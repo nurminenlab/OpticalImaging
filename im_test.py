fce3a3fcc6111648f56189eb899770dca6427b3a
nurminenlab <lonurmin@central.uh.edu>
2023-04-04 21:01:06 -0500

    Tried figuring out tiff problems

---
+++ im_test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+im_fold = 'C:/Users/lonurmin/Desktop/repos/OpticalImaging/'
+im_name = '0002.tiff'
+
+im = Image.open(im_fold+im_name)
+
