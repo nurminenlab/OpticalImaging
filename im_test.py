ec3763545aa08f8ccab0fa5c03b9e82a6f718cd3
Sandhya Ammasi <60815555+sandhyaammasi@users.noreply.github.com>
2023-04-05 16:00:40 -0500

    Delete im_test.py

--- im_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-
-im_fold = 'C:/Users/lonurmin/Desktop/repos/OpticalImaging/'
-im_name = '0002.tiff'
-
-im = Image.open(im_fold+im_name)
-
