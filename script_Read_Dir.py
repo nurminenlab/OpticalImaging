5753665203f4a369c496c128596ab73833c7379b
Sandhya Ammasi <sandhyaammasi@gmail.com>
2023-04-12 17:07:00 -0500

    add changeable parameter(no. of imgs)

--- script_Read_Dir.py
+++ script_Read_Dir.py
@@ -18,21 +18,22 @@ while True:
     # save only new file
     new_files = list(set(tiff_files) - existing_files)    
 
+    parameter = 3
 
     # print only new file
     for tiff_file in new_files:
         print(f"New file created: {tiff_file}")
         images_folder.append(tiff_file)
         for i, num in enumerate(images_folder):   
-            if i % 12 < 3:
+            if i % parameter*4 < parameter:         # if i % 12 < 3
                 baseLine_SC1.append(num)
                 baseLine_SC1 = list(OrderedSet(baseLine_SC1))
 
-            elif i % 12 < 6:
+            elif i % parameter*4 < parameter*2:     #i % 12 < 6:
                 SC1.append(num)
                 SC1 = list(OrderedSet(SC1))
 
-            elif i % 12 < 9:
+            elif i % parameter*4 < parameter*3:     #i % 12 < 9
                 baseLine_SC2.append(num)
                 baseLine_SC2 = list(OrderedSet(baseLine_SC2))
 
